ea51875edcbc5c1e262258990b31eba909470b0d
yumotu <100271226+yumotu@users.noreply.github.com>
2022-07-07 12:54:11 +0800

    Changes_test_col

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -62,6 +62,7 @@ TEST(Jumptest, Gametest){
   EXPECT_EQ(-6,player->GetVy());
   game.CloseMap();
 }
+TEST(Colitest, Gametest){}
 int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
